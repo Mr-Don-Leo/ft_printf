f0817e6c88810b1a37e849bdf04dffad9e4530e4
Mr-Don-Leo <maksimababayan@gmail.com>
2023-12-03 12:07:37 +0400

    Update libftprintf.h

---
+++ libftprintf.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbabayan <mbabayan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/03 11:49:10 by mbabayan          #+#    #+#             */
+/*   Updated: 2023/12/03 11:52:53 by mbabayan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+#define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <stddef.h>
+# include <unistd.h>
+
+#endif FT_PRINTF_H
